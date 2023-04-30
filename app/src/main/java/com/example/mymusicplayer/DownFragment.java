b4f36d4986ff261c14ebea388d11f03ab74f4bc9
olkovko <olkov.kir1@yandex.ru>
2023-04-30 14:31:08 +0300

    redesigned toolbar and fragments

---
+++ app/src/main/java/com/example/mymusicplayer/DownFragment.java
@@ -0,0 +1,22 @@
+package com.example.mymusicplayer;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class DownFragment extends Fragment {
+
+    public DownFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_down, container, false);
+    }
+}
--- app/src/main/java/com/example/mymusicplayer/MainActivity.java
+++ app/src/main/java/com/example/mymusicplayer/MainActivity.java
@@ -1,36 +1,17 @@
 package com.example.mymusicplayer;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.viewpager2.adapter.FragmentStateAdapter;
 
 import android.os.Bundle;
 
 import com.example.mymusicplayer.databinding.ActivityMainBinding;
-import com.google.android.material.tabs.TabLayout;
-import com.google.android.material.tabs.TabLayoutMediator;
 
 public class MainActivity extends AppCompatActivity {
-
-
-    int[] myImageList = new int[]{R.drawable.icon_music, R.drawable.icon_lovely, R.drawable.icon_acc};
-
-    String[] names = new String[]{"Музыка", "Любимое", "Профиль"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         ActivityMainBinding binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
-        FragmentStateAdapter pageAdapter = new MyAdapter(this);
-        binding.pager.setAdapter(pageAdapter);
-        TabLayoutMediator tabLayoutMediator = new TabLayoutMediator(binding.tabLayout, binding.pager, new TabLayoutMediator.TabConfigurationStrategy() {
-            @Override
-            public void onConfigureTab(@NonNull TabLayout.Tab tab, int position) {
-                    tab.setText(names[position]);
-                    tab.setIcon(myImageList[position]);
-            }
-        });
-        tabLayoutMediator.attach();
     }
 }
